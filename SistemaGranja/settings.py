ed0c6da2ad051bf22b3ac2686e586fb15b3c122f
ipereto <ivanfelipeprz@gmail.com>
2016-09-11 00:44:56 -0500

    Se crea el modelo de Granja, se habilita el sitio de administración, se crea un superusuario para acceder, se crean un registro de granja, se modifica la forma de ver un objeto en el sitio de administración y se crean filtros de búsqueda

--- SistemaGranja/settings.py
+++ SistemaGranja/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'granja.apps.GranjaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- granja/apps.py
+++ granja/apps.py
@@ -5,3 +5,4 @@ from django.apps import AppConfig
 
 class GranjaConfig(AppConfig):
     name = 'granja'
+    
