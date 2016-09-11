59696b7eca44ae71a052e9045fb2cf32dd62b902
ipereto <ivanfelipeprz@gmail.com>
2016-09-11 02:01:40 -0500

    Se integra un template con una vista habilitada para ser accedida desde una URL

--- granja/views.py
+++ granja/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from .models import Granja
 
 # Create your views here.
+def hello_world(request):
+    #return HttpResponse('Hello world!')
+    #return render(request, 'index.html')
+    granja = Granja.objects.order_by('id')
+    template = loader.get_template('index.html')
+    context = {
+    'granja' : granja
+    }
+    return HttpResponse(template.render(context,request))
